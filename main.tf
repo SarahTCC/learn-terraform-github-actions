32a689ec279c82160d2075688a9b8ab2475381a9
Sarah Woolhouse <119283073+SarahTCC@users.noreply.github.com>
2023-04-19 11:11:42 +1000

    Update main.tf
    Add TCC org

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "TheCommerceCo"
 
     workspaces {
       name = "learn-terraform-github-actions"
